70d8a796d7c37e38f99471bfbc31f5e82f1ed31a
aimable <aganzerizen@gmail.com>
2024-06-02 23:14:09 +0200

    2 June 24 11:14 PM

--- Frontend/src/pages/smdevice/smChat.jsx
+++ Frontend/src/pages/smdevice/smChat.jsx
@@ -45,7 +45,7 @@ const navigate = useNavigate();
 // console.log('authUser :', AuthUser._id)
   return (
     <div className='w-screen'>
-        <div className='hdr w-full flex h-3/4  relative bg-base-100' style={{zIndex: '1'}}>
+        <div className='hdr w-full flex  relative bg-base-100' style={{zIndex: '1'}}>
             <div className='hdr-ct px-2 py-1 w-full fixed bg-base-100  flex flex-row justify-between'>
                 <div className='pfl flex  flex-row gap-2'>
                     <div className='flex  justify-center self-center' onClick={() => handleReset(selectedUser)}><KeyboardArrowLeftIcon /></div>
